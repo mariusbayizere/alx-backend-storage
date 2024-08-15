78ccea194b246ff9fa142c654dacbf958455b33c
mariusbayizere <bayizeremarius119@gmail.com>
2024-08-15 06:26:35 +0200

    0x01. NoSQL

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Module to interact with Redis and store/retrieve data.
 """
 
-from typing import Union
+from typing import Union, Callable, Optional
 from uuid import uuid4
 import redis
 
@@ -33,3 +33,50 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieves data from Redis using the provided key and an optional
+        conversion function.
+
+        Args:
+            key (str): The key to retrieve data.
+            fn (Callable, optional): A callable used to convert the data to
+            the desired format.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data,
+            possibly converted using fn.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieves a string from Redis using the provided key.
+
+        Args:
+            key (str): The key to retrieve the string.
+
+        Returns:
+            Optional[str]: The retrieved string, or None if key doesn't exist.
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieves an integer from Redis using the provided key.
+
+        Args:
+            key (str): The key to retrieve the integer.
+
+        Returns:
+            Optional[int]: The retrieved integer, or None if key doesn't exist.
+        """
+        return self.get(key, int)
